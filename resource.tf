1fdac731a6df755ba1d0bec0288f2254fd581d3c
Jahnavi-Mandla <jahnavimandla939@gmail.com>
2025-07-16 17:39:57 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -2,7 +2,6 @@ resource "azurerm_resource_group" "Assignterra" {
     name     = "example-resources"
     location = "East US"
 }
-
 resource "azurerm_virtual_network" "terra_vnet" {
     name                = "terra-vnet"
     address_space       = ["10.0.0.0/16"]
