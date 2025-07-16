3fe06b33e2eb8ab248b1157b9206e809c49cd89c
Jahnavi-Mandla <jahnavimandla939@gmail.com>
2025-07-16 17:27:24 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "storeterra" {
+resource "azurerm_resource_group" "Assignterra" {
     name     = "example-resources"
     location = "East US"
 }
@@ -6,20 +6,20 @@ resource "azurerm_resource_group" "storeterra" {
 resource "azurerm_virtual_network" "terra_vnet" {
     name                = "terra-vnet"
     address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.terra.location
-    resource_group_name = azurerm_resource_group.terra.name
+    location            = azurerm_resource_group.Assignterra.location
+    resource_group_name = azurerm_resource_group.Assignterra.name
 }
  
 resource "azurerm_subnet" "subnet1" {
     name                 = "subnet1"
-    resource_group_name  = azurerm_resource_group.terra.name
+    resource_group_name  = azurerm_resource_group.Assignterra.name
     virtual_network_name = azurerm_virtual_network.terra_vnet.name
     address_prefixes     = ["10.0.1.0/24"]
 }
  
 resource "azurerm_subnet" "subnet2" {
     name                 = "subnet2"
-    resource_group_name  = azurerm_resource_group.terra.name
+    resource_group_name  = azurerm_resource_group.Assignterra.name
     virtual_network_name = azurerm_virtual_network.terra_vnet.name
     address_prefixes     = ["10.0.2.0/24"]
 }
